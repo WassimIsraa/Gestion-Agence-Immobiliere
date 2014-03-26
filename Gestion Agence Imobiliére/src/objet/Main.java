3583d11c63b521c18a21d74a7ff9b3a65d8df512
Faidiwassim <faidiwassim@gmail.com>
2014-03-26 18:19:42 +0100

    26/3/2014

--- Gestion Agence Imobiliére/src/objet/Main.java
+++ Gestion Agence Imobiliére/src/objet/Main.java
@@ -14,14 +14,15 @@ public class Main
 	{
 		//System.out.println("mchetttt");	
 	ClientDAO cdao=new ClientDAO();
-	System.out.println("mchetttt");	
+		
 	
-	Client client1=new Client(1,"Wassim");
+	Client client1=new Client(2,"WWassim");
 	//Client client2=new Client(2,"fedi");
 	//Client client3=new Client(3,"wael");
 cdao.save(client1);
-List<Client> l=cdao.getClient();
-System.out.println(l.get(0).getId());
+System.out.println("mchetttt");
+//List<Client> l=cdao.getClient();
+//System.out.println(l.get(0).getId());
 
 	/*cdao.save(client2);cdao.save(client3);*/
 	
